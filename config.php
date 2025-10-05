939f1adf863af42d2fbda9e8b772facf9e283b24
Wangska <wangska1283@gmail.com>
2025-10-05 22:40:04 +0800

    changes nadsa

--- config.php
+++ config.php
@@ -6,6 +6,11 @@ $db_pass = getenv('DB_PASSWORD') ?: "";
 $db_name = getenv('DB_DATABASE') ?: "noteit_db";
 $db_port = getenv('DB_PORT') ? intval(getenv('DB_PORT')) : 3306;
 
+// Force TCP when using 'localhost' to avoid missing Unix socket inside containers
+if ($db_host === 'localhost') {
+    $db_host = '127.0.0.1';
+}
+
 // Create database connection
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
